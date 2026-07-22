4e95f3a4499dc7112c50e9897ed43ae4d27ae24d
Archita-singha2k24 <architasingha31@gmail.com>
2026-07-22 09:50:21 +0530

    fix: successful build after adding default export

--- src/hooks/useSWR.js
+++ src/hooks/useSWR.js
@@ -77,3 +77,5 @@ export function useSWR(key, fetcher, { ttl = 5 * 60 * 1000 } = {}) {
 
   return { data: displayData, error: displayError, isValidating: displayIsValidating, mutate };
 }
+
+export default useSWR;
