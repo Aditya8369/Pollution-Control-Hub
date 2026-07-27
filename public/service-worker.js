364c9ce5e4a5d89c508f19951f2349fc5e032e48
Revati Kadam <revatikadam0607@gmail.com>
2026-07-27 20:34:56 +0530

    issue 317

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pch-cache-v1';
+const APP_SHELL = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
