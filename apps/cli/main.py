7ff0dad0bf052f63e670c8743555fd1b000db110
rudresh69 <borlerudresh1911@gmail.com>
2026-08-14 22:38:13 +0530

    Fix #720: Centralize Constants

---
+++ apps/cli/main.py
@@ -0,0 +1,12 @@
+--- a/apps/cli/main.py
+@@ -1,5 +1,7 @@
+ import os
+ import sys
++from typing import List
+ 
+ def main():
+     # Existing code remains the same
+@@ -10,6 +12,8 @@ def main():
+     # Additional code can be added here
++
++    print("Running CLI application")
